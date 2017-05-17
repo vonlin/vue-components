a42dec8386218f39bee67fdfa7628ad8fc6b8cb4
fengchunlin <fengchunlin@myhome.163.com>
2017-05-17 18:37:44 +0800

    update

---
+++ src/components/dateTime/index.vue
@@ -0,0 +1,366 @@
+<template>
+  <div v-bind:class="cshow">
+    <div class="dt-opt">
+      <span class="fl" v-on:touchstart="toYMD" v-show="!currType">&lt;年日月</span>
+      <span class="dt-confirm" v-on:touchstart="confirm">完成</span>
+      <span class="fr" v-on:touchstart="toHSM" v-show="currType">时分秒&gt;</span>
+    </div>
+    <div class="mask"></div>
+    <div class="selectRect"></div>
+    <div class="dt-container-wrap">
+      <div class="dt-container" v-bind:class="swipeClass">
+        <!--<dtcell v-for="col in cols" v-bind:coltype="col"></dtcell>-->
+        <div class="dt-cell-col" v-for="col in coltypes">
+          <ul v-bind:coltype="col" v-datescroll="scrollEnd" v-bind:data-scrolltop="scrolltopvalue(col)">
+            <li class="dt-cell-row" v-for="data in getCols(col)">{{data}}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import $Util from '../../assets/javascripts/util'
+  import '../../directives/datescroll'
+  var targetEl = null;
+  export default {
+    name: 'DateTime',
+    props: {
+      cshow: '',
+      target: ''
+    },
+    methods: {
+      getCols (coltype) {
+        return this.coldatas[coltype].datas;
+      },
+      setDays (selected, coltype) {
+        let _tempDays = $Util.generateDays;
+        selected = Math.round(selected)
+        if (coltype == 'M') {
+          //根据月份初始化天数
+          if ([4, 6, 9, 11].indexOf(selected + 1) != -1) {
+            this.coldatas.D.datas = _tempDays.evenDatas;
+          } else if ([1, 3, 5, 7, 8, 10, 12].indexOf(selected + 1) != -1) {
+            this.coldatas.D.datas = _tempDays.oddDatas;
+          } else if ((selected + 1) == 2) {
+            //2月份
+            if ((!this.selectYear % 4 && this.selectYear % 100 != 0) || !this.selectYear % 400) {
+              //闰年
+              this.coldatas.D.datas = _tempDays.datas2days29;
+            } else {
+              //平年
+              this.coldatas.D.datas = _tempDays.datas2days28;
+            }
+          }
+        } else if (coltype == 'Y' && this.selectMonth == '02') {
+          //2月份
+          if ((!this.selectYear % 4 && this.selectYear % 100 != 0) || !this.selectYear % 400) {
+            //闰年
+            this.coldatas.D.datas = _tempDays.datas2days29;
+          } else {
+            //平年
+            this.coldatas.D.datas = _tempDays.datas2days28;
+          }
+        }
+
+      },
+      toHSM () {
+        this.swipeClass = "swipeLeft";
+        this.currType = !this.currType;
+      },
+      toYMD () {
+        this.swipeClass = "swipeRight";
+        this.currType = !this.currType;
+      },
+      confirm (e) {
+        targetEl = targetEl || document.getElementById(this.target);
+        targetEl.innerText = this.selectYear + "年" + this.selectMonth + "月" + this.selectDay + "日" + this.selectHour + "时" + this.selectMinute + "分" + this.selectSecond + "秒";
+        this.cshow = 'fadeOut';
+      },
+      scrolltopvalue (coltype) {
+        let _index = this.coldatas[coltype].datas.indexOf(this.coldatas[coltype].curr) - 3;
+        return (_index >= 0 ? -_index : _index) * 28;
+      },
+      scrollEnd (e) {
+        let coltype = e.el.getAttribute("coltype");
+        let selected = Math.round((84 - e.moveDisY) / 28);
+        //获取年，月，日
+        if (coltype == "Y") {
+          this.selectYear = this.coldatas.Y.datas[selected];
+        } else if (coltype == "M") {
+          this.selectMonth = this.coldatas.M.datas[selected];
+        } else if (coltype == "D") {
+          this.selectDay = this.coldatas.D.datas[selected];
+        } else if (coltype == "H") {
+          this.selectHour = this.coldatas.H.datas[selected];
+        } else if (coltype == "MI") {
+          this.selectMinute = this.coldatas.MI.datas[selected];
+        } else if (coltype == "S") {
+          this.selectSecond = this.coldatas.S.datas[selected];
+        }
+        this.setDays(selected, coltype);
+      }
+    },
+    data () {
+      return {
+        coltypes: ["Y", "M", "D", "H", "MI", "S"],
+        coldatas: {
+          Y: $Util.generateYears(),
+          M: $Util.generateMonths,
+          D: $Util.generateDays,
+          H: $Util.generateHours,
+          MI: $Util.generateMinutes,
+          S: $Util.generateSeconds
+        },
+        selectYear: $Util.generateYears().curr,
+        selectMonth: $Util.generateMonths.curr,
+        selectDay: $Util.generateDays.curr,
+        selectHour: $Util.generateHours.curr,
+        selectMinute: $Util.generateMinutes.curr,
+        selectSecond: $Util.generateSeconds.curr,
+
+        maskYColTypes: [2017, 2017, 2017, 2017, 2017, 2017],
+
+        swipeClass: "",
+        currType: true
+      };
+    },
+    computed: {
+      maskTypes () {
+        var maskTypes = [];
+        var _datas = this.coldatas.Y.datas;
+        if (this.currType) {
+          //年月日
+          maskTypes = ["Y", "M", "D"];
+
+          var yIndex = this.coldatas.Y.datas.indexOf(this.selectYear);
+          var mIndex = this.coldatas.M.datas.indexOf(this.selectMonth);
+          var dIndex = this.coldatas.D.datas.indexOf(this.selectDay);
+          this.maskYColTypes.push(this.coldatas.Y.datas[yIndex - 3]);
+          this.maskYColTypes.push(this.coldatas.Y.datas[yIndex - 2]);
+          this.maskYColTypes.push(this.coldatas.Y.datas[yIndex - 1]);
+          this.maskYColTypes.push(this.coldatas.Y.datas[yIndex]);
+          this.maskYColTypes.push(this.coldatas.Y.datas[yIndex + 1]);
+          this.maskYColTypes.push(this.coldatas.Y.datas[yIndex + 2]);
+          this.maskYColTypes.push(this.coldatas.Y.datas[yIndex + 3]);
+        } else {
+          //时分秒
+          maskTypes = ["H", "MI", "S"];
+        }
+        return maskTypes;
+      },
+      maskColTypes () {
+        var maskColTypes;
+
+      }
+    }
+  }
+</script>
+
+<style scoped="scoped">
+  .fadeIn {
+    animation: fadeIn .3s ease-out;
+    width: 100%;
+    height: 240px;
+    position: fixed;
+    bottom: 0;
+  }
+
+  .fadeOut {
+    animation: fadeOut .3s ease-out;
+    width: 100%;
+    height: 240px;
+    position: fixed;
+    bottom: -240px;
+  }
+
+  @-webkit-keyframes fadeIn {
+    from {
+      bottom: -240px;
+    }
+    to {
+      bottom: 0;
+    }
+  }
+
+  @-webkit-keyframes fadeOut {
+    from {
+      bottom: 0;
+    }
+    to {
+      bottom: -240px;
+    }
+  }
+
+  .dt-container {
+    width: 200%;
+    height: 200px;
+    z-index: 1;
+    position: relative;
+    bottom: 0;
+    display: -webkit-box;
+    overflow: hidden;
+  }
+
+  .dt-container-wrap {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    overflow: hidden;
+  }
+
+  .mask {
+    width: 100%;
+    height: 200px;
+    position: absolute;
+    z-index: 10;
+    bottom: 0;
+    left: 0;
+    overflow: hidden;
+    pointer-events: none;
+    background: -webkit-linear-gradient(top, rgb(255, 255, 255) 0, rgba(255, 255, 255, 0.5) 40%, rgba(255, 255, 255, 0) 41%, rgba(255, 255, 255, 0) 54%, rgba(255, 255, 255, 0.5) 55%, rgb(255, 255, 255) 100%);
+  }
+
+  .selectRect {
+    width: 100%;
+    height: 34px;
+    border-top: 1px solid #e9e9e9;
+    border-bottom: 1px solid #e9e9e9;
+    position: absolute;
+    bottom: 83px;
+    z-index: 11;
+    font-size: 24px;
+    pointer-events: none;
+  }
+
+  .dt-confirm {
+    position: absolute;
+    left: 45%;
+  }
+
+  .dt-opt {
+    width: 100%;
+    border-top: 1px solid #bbb;
+    position: absolute;
+    height: 40px;
+    text-align: center;
+    line-height: 40px;
+    padding-left: 20px;
+    padding-right: 20px;
+    background: #3bb593;
+    bottom: 200px;
+    font-size: 14px;
+    color: #fff;
+  }
+
+  .dt-cell-col {
+    display: block;
+    -webkit-box-flex: 1;
+    box-flex: 1;
+    text-align: center;
+    height: 200px;
+    overflow: hidden;
+    width: 0.16666666666666666%;
+    font-size: 20px;
+  }
+
+  .dt-cell-row {
+    padding: 5px 10px 5px;
+    height: 28px;
+  }
+
+  .swipeLeft {
+    -webkit-transition: transform .6s;
+    transition: transform .6s;
+    -webkit-transform: translateX(-50%);
+    transform: translateX(-50%);
+  }
+
+  .swipeRight {
+    -webkit-transition: transform .6s;
+    transition: transform .6s;
+    -webkit-transform: translateX(0);
+    transform: translateX(0);
+  }
+
+  .mask ul li:nth-child(4), .curr-select {
+    font-size: 21px;
+  }
+
+  .curr-select-prev-5 {
+    /*font-size:12px;*/
+    /*color:rgb(244,244,244);*/
+    /*height:auto;*/
+  }
+
+  .curr-select-prev-4 {
+    /*font-size:16px;*/
+    /*color:rgb(234,234,234);*/
+    /*height:auto;*/
+  }
+
+  .curr-select-prev-3, .curr-select-prev-2, .curr-select-prev-1 ,.curr-select-after-3,.curr-select-after-2,.curr-select-after-1{
+    -webkit-transition: opacity 1s, transform;
+  }
+
+  .curr-select-prev-3 {
+    /*font-size:18px;*/
+    /*color:rgb(224,224,224);*/
+    /*transform: scaleY(0.4);*/
+    /*height:20px;*/
+    -webkit-transform: rotateX(63deg) translateZ(-6px);
+  }
+
+  .curr-select-prev-2 {
+    /*font-size:20px;*/
+    /*color:rgb(214,214,214);*/
+    /*transform: scaleY(0.6);*/
+    /*height:auto;*/
+    -webkit-transform: rotateX(37deg) translateZ(0px);
+  }
+
+  .curr-select-prev-1 {
+    /*font-size:22px;*/
+    /*color:rgb(204,204,204);*/
+    /*transform: scaleY(0.9);*/
+    /*height:auto;*/
+    -webkit-transform: rotateX(2deg) translateZ(1px);
+  }
+
+  .curr-select-after-5 {
+    /*font-size:12px;*/
+    /*color:rgb(244,244,244);*/
+    /*height:auto;*/
+  }
+
+  .curr-select-after-4 {
+    /*font-size:16px;*/
+    /*color:rgb(234,234,234);*/
+    /*height:auto;*/
+  }
+
+  .curr-select-after-3 {
+    /*font-size:18px;*/
+    /*color:rgb(224,224,224);*/
+    /*transform: scaleY(0.4);*/
+    -webkit-transform: rotateX(-63deg) translateZ(-6px);
+  }
+
+  .curr-select-after-2 {
+    /*font-size:20px;*/
+    /*color:rgb(214,214,214);*/
+    /*transform: scaleY(0.6);*/
+    /*height:auto;*/
+    -webkit-transform: rotateX(-37deg) translateZ(0px);
+  }
+
+  .curr-select-after-1 {
+    /*font-size:22px;*/
+    /*color:rgb(204,204,204);*/
+    /*transform: scaleY(0.9);*/
+    /*height:auto;*/
+    -webkit-transform: rotateX(-2deg) translateZ(-1px);
+  }
+
+</style>
--- src/directives/datescroll.js
+++ src/directives/datescroll.js
@@ -3,12 +3,13 @@
  */
 import Vue from 'vue'
 
-Vue.directive('datescroll',{
-  bind: function(el,binding,vonde){
+Vue.directive('datescroll', {
+  bind: function (el, binding, vonde) {
     //定位当前日期
-    el.style.transform = "translateY("+parseInt(el.dataset.scrolltop) + "px)";
+    el.style.transform = "translateY(" + parseInt(el.dataset.scrolltop) + "px)";
     setMaskVal(el.dataset.scrolltop);
-    let startX,startY,endX,endY = 0,moveDisY=0,touchendY=0,isFirstTouch=true,
+    let startX, startY, endX, endY = 0, moveDisY = 0, touchendY = 0, isFirstTouch = true,
+      needEndTime,//最后一段滚动距离所需时间
       //最大滚动高度
       maxScrollH;
 
@@ -18,128 +19,115 @@ Vue.directive('datescroll',{
       stopMove,//是否停止滚动
       startTime;//记录开始触摸时间
 
-    var currel = null,
-        currel1 =null,
-        currel2 =null,
-        currel3 =null,
-        currel4 =null,
-        currel5 =null,
-        currel6 =null;
-
-    function setMaskVal(disY){
+    function setMaskVal(disY) {
       var tempELs = el.childNodes;
-      for(let childNode of tempELs){
-        childNode.className="dt-cell-row";
+      for (let childNode of tempELs) {
+        childNode.className = "dt-cell-row";
       }
       var selectNum = Math.round((84 - disY) / 28);
-        if(tempELs[selectNum]) tempELs[selectNum].className += " curr-select";
-        if(tempELs[selectNum-1]) tempELs[selectNum-1].className += " curr-select-round-1";
-        if(tempELs[selectNum-2]) tempELs[selectNum-2].className += " curr-select-round-2";
-        if(tempELs[selectNum-3]) tempELs[selectNum-3].className += " curr-select-round-3";
-        if(tempELs[selectNum-4]) tempELs[selectNum-4].className += " curr-select-round-4";
-        if(tempELs[selectNum-5]) tempELs[selectNum-5].className += " curr-select-round-5";
+      if (tempELs[selectNum]) tempELs[selectNum].className += " curr-select";
+      if (tempELs[selectNum - 1]) tempELs[selectNum - 1].className += " curr-select-prev-1";
+      if (tempELs[selectNum - 2]) tempELs[selectNum - 2].className += " curr-select-prev-2";
+      if (tempELs[selectNum - 3]) tempELs[selectNum - 3].className += " curr-select-prev-3";
 
 
-        if(tempELs[selectNum+1]) tempELs[selectNum+1].className += " curr-select-round-1";
-        if(tempELs[selectNum+2]) tempELs[selectNum+2].className += " curr-select-round-2";
-        if(tempELs[selectNum+3]) tempELs[selectNum+3].className += " curr-select-round-3";
-        if(tempELs[selectNum+4]) tempELs[selectNum+4].className += " curr-select-round-4";
-        if(tempELs[selectNum+5]) tempELs[selectNum+5].className += " curr-select-round-5";
+      if (tempELs[selectNum + 1]) tempELs[selectNum + 1].className += " curr-select-after-1";
+      if (tempELs[selectNum + 2]) tempELs[selectNum + 2].className += " curr-select-after-2";
+      if (tempELs[selectNum + 3]) tempELs[selectNum + 3].className += " curr-select-after-3";
 
     }
 
-    el.addEventListener("touchstart",function(e){
-      e.cancelBubble = true;
+    el.addEventListener("touchstart", function (e) {
+      el.style.transition = "";
       startX = e.changedTouches[0].pageX;
       startY = e.changedTouches[0].pageY;
 
       startTime = Date.now();
       lastY = startY;
+      e.preventDefault();
+    }, false);
 
-      currel = document.getElementById("curr-select"),
-        currel1 = document.getElementById("curr-select-round-1"),
-        currel2 = document.getElementById("curr-select-round-2"),
-        currel3 = document.getElementById("curr-select-round-3"),
-        currel4 = document.getElementById("curr-select-round-4"),
-        currel5 = document.getElementById("curr-select-round-5"),
-        currel6 = document.getElementById("curr-select-round-6");
-    },{passive:false});
-
-    el.addEventListener("touchmove",function(e){
-      e.cancelBubble = true;
+    el.addEventListener("touchmove", function (e) {
       endX = e.changedTouches[0].pageX;
       endY = e.changedTouches[0].pageY;
-      if(isFirstTouch){
+      if (isFirstTouch) {
         //定位当前日期
-        moveDisY = endY - startY + touchendY +parseInt(el.dataset.scrolltop) ;
-      }else{
+        moveDisY = endY - startY + touchendY + parseInt(el.dataset.scrolltop);
+      } else {
         moveDisY = endY - startY + touchendY;
       }
       setMaskVal(moveDisY);
       el.style.transform = "translateY(" + moveDisY + "px)";
-    },{passive:false});
+      e.preventDefault();
+    }, false);
 
-    el.addEventListener("touchend",function(e){
-      e.cancelBubble = true;
+    el.addEventListener("touchend", function (e) {
       isFirstTouch = false;
       //记录松开时的滚动位置
       //如果小于一栏的高度，则偏移为0
-      if(moveDisY <28 && moveDisY > 0) moveDisY = 0;
+      if (moveDisY < 28 && moveDisY > 0) moveDisY = 0;
       touchendY = moveDisY || 0;
       maxScrollH = (el.childNodes.length - 4) * 28
 
-      function setTransY(){
-        el.style.transform = "translateY("+touchendY+"px)";
+      function setTransY() {
+        el.style.transition = "transform .8s ease-out";
+        el.style.transform = "translateY(" + touchendY + "px)";
         setMaskVal(touchendY);
-        binding.value.call(null,{
+        binding.value.call(null, {
           el: el,
           moveDisY: touchendY
         });
       }
 
-      if(touchendY > 3* 28){
+      if (touchendY > 3 * 28) {
         //拖动到最上面那条数据 不能再拖动
-        touchendY= 84;
+        touchendY = 84;
         setTransY();
-      }else if(touchendY < -maxScrollH){
+      } else if (touchendY < -maxScrollH) {
         //拖动到最下面那条数据 不能再拖动
-        touchendY= -maxScrollH;
+        touchendY = -maxScrollH;
         setTransY();
-      }else {
+      } else {
         //缓动效果
         el.style.transform = "translateY(" + moveDisY + "px)";
 
         var endTime = Date.now();
         nowY = e.changedTouches[0].pageY;
         v = (nowY - lastY) / (endTime - startTime);
-        (function(v, startTime, contentY) {
+        (function (v, startTime, contentY) {
           var dir = v > 0 ? -1 : 1; //加速度方向
-          var deceleration = dir*0.0006;
+          var deceleration = dir * 0.0006;
           var duration = v / deceleration; // 速度消减至0所需时间
           var dist = v * duration / 2; //最终移动多少
-          function inertiaMove() {
-            if(stopMove) return;
-            var nowTime = Date.now();
-            var t = nowTime-startTime;
-            var nowV = v + t*deceleration;
+          dist = Math.ceil(dist / 28) * 28;
+          var _t = 2 * dist / v;
 
-            var moveY = (v + nowV)/2 * t;
+          var lastV = v;
+
+          function inertiaMove() {
+            if (stopMove) return;
+            var nowTime = Date.now();
+            var t = nowTime - startTime;
+            var nowV = v + t * deceleration;
+
+            var moveY = (v + nowV) / 2 * t;
             var tempY = contentY + moveY;
             touchendY = tempY;
-            if(touchendY > 3* 28){
+            if (touchendY > 3 * 28) {
               //拖动到最上面那条数据 不能再拖动
-              touchendY= 84;
+              touchendY = 84;
               setTransY();
               return;
-            }else if(touchendY < -maxScrollH){
+            } else if (touchendY < -maxScrollH) {
               //拖动到最下面那条数据 不能再拖动
-              touchendY= -maxScrollH;
+              touchendY = -maxScrollH;
               setTransY();
               return;
             }
+
             // 速度方向变化表示速度达到0了
-            if(dir*nowV > 0) {
-              tempY = tempY - Math.round(tempY%28);
+            if (dir * nowV > 0) {
+              tempY = Math.round(tempY / 28) * 28;
               touchendY = tempY;
               setTransY();
               return;
@@ -148,10 +136,12 @@ Vue.directive('datescroll',{
             setMaskVal(tempY);
             requestAnimationFrame(inertiaMove);
           }
+
           inertiaMove();
         })(v, endTime, moveDisY);
       }
+      e.preventDefault();
 
-    },{passive:false});
+    }, false);
   }
 });
